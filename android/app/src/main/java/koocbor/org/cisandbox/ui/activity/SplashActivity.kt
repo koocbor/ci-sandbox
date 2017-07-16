f79059d49b96a48692c7a43114f7b97b43fcb2ca
koocbor <robcook@gmail.com>
2017-07-16 07:43:39 -0400

    android travis attempt 1

---
+++ android/app/src/main/java/koocbor/org/cisandbox/ui/activity/SplashActivity.kt
@@ -0,0 +1,16 @@
+package koocbor.org.cisandbox.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import koocbor.org.cisandbox.R
+
+/**
+ * Created by robcook on 7/16/17.
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
